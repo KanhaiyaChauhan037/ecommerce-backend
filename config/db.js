909ad5c0d950876a8934997db3ea69f38777f38d
kanhaiya chauhan <kanhaiyachauhan037@gmail.com>
2023-06-20 00:16:36 +0530

    projects done

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+    }
+};
+
+module.exports = connect;
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const cors = require("cors")
+const Razorpay = require("razorpay");
+const userRouter = require("./routes/users.route")
+const productRouter = require("./routes/product.route")
+const cartRouter = require("./routes/cart.route")
+const app = express()
+require("dotenv").config()
+
+const connect = require("./config/db")
+const addressRouter = require("./routes/address.route")
+const P = process.env.PORT;
+
+app.use(cors())
+app.use(express.json());
+app.use("/users", userRouter)
+app.use("/products", productRouter)
+app.use("/carts", cartRouter)
+app.use("/admin", addressRouter)
+
+app.get("/", (req, res) => {
+    res.send("Home page")
+})
+
+var instance = new Razorpay({
+    key_id: process.env.ROZAR_ID,
+    key_secret: process.env.ROZAR_SECRET,
+});
+
+app.post("/rozar-order", async (req, res) => {
+    var options = {
+        amount: req.body.amount,
+        currency: "INR",
+        receipt: "order_rcptid_11",
+    };
+
+    try {
+        const order = await instance.orders.create(options);
+        res.send(order);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+
+app.listen(P, async () => {
+    await connect()
+    console.log(`server is started on http://localhost:${P}`)
+})
